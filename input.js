e388bb3e546793d3e8371c077a726a74fa8c670f
kadhichawal <157056471+kadhichawal@users.noreply.github.com>
2025-03-28 22:51:57 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,10 @@
+function toggleDropdown() {
+    var show = document.getElementById("show");
+    
+    if (show.style.display === "block") {
+        show.style.display = "none";
+    } else {
+        show.style.display = "block";
+    }
+}
+ 
